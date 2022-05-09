8ceb4753a1b1a45a2f7aee3da382961477d142d9
立蓁 陳 <B10956029@office365.npust.edu.tw>
2022-05-09 11:52:15 +0800

    加入專案檔案。

---
+++ 0509/TestDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _0509
+{
+    public class TestDB:DbContext
+    {
+        public TestDB(DbContextOptions<TestDB> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
